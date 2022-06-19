0e71b614bc1c154ea88c0838269905cbe26d81ad
Dmitriy betterHell <skkogame@gmail.com>
2022-06-19 17:23:34 +0300

    separate on 2 js scripts

--- loadCards.js
+++ loadCards.js
@@ -55,39 +55,3 @@ const loadCards = () => {
 };
 
 loadCards();
-
-const defaultWeight = document.querySelectorAll(".card__weight");
-
-defaultWeight.forEach((weight) => {
-  weight.style.backgroundColor = "rgb(73, 179, 236)";
-});
-
-const likeCards = () => {
-  const cards = document.querySelectorAll(".card");
-
-  cards.forEach((card) => {
-    const cardImgDefault = card.querySelector(".card__img");
-    const weight = card.querySelector(".card__weight");
-    const postDescription = card.querySelector(".card__postCardInfo");
-
-    card.addEventListener("click", () => {
-      if (weight.style.backgroundColor === "rgb(73, 179, 236)") {
-        cardImgDefault.setAttribute(
-          "src",
-          "images/background/Photo_selected.png"
-        );
-        postDescription.innerHTML = `В корзине!`;
-        weight.style.backgroundColor = "rgb(231, 45, 135)";
-      } else if ((weight.style.backgroundColor = "rgb(231, 45, 135)")) {
-        cardImgDefault.setAttribute(
-          "src",
-          "images/background/Photo_default.png"
-        );
-        postDescription.innerHTML = `Чего сидишь? Порадуй котэ, <a href="##">купи.</a>`;
-        weight.style.backgroundColor = "rgb(73, 179, 236)";
-      }
-    });
-  });
-};
-
-likeCards();
